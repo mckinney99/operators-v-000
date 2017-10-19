0a0c398815b0df823bd6c041c9e28716581a5357
Eric McKinney <mckinney99@gmail.com>
2017-10-19 18:42:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,11 @@
 def unsafe?(speed)
-speed < 40 || speed > 60 ? true : false
+if speed < 40 || speed > 60  
+	return true 
+else false
 end
 
 
 
 def not_safe?(speed)
-
+speed < 40 || speed > 60 ? true : false
 end
